5d9496eb096fec1b91d876f3ab119f53b19d3cbe
Renan Grande <renanlgrande@hotmail.com>
2026-01-11 00:24:14 -0300

    update teste

--- src/test/java/com/fiap/producao/worker/PedidoPagoListenerTest.java
+++ src/test/java/com/fiap/producao/worker/PedidoPagoListenerTest.java
@@ -4,54 +4,43 @@ import com.fiap.producao.domain.dto.PedidoPagoEvento;
 import com.fiap.producao.domain.entity.PedidoProducao;
 import com.fiap.producao.domain.entity.StatusPedido;
 import com.fiap.producao.repository.PedidoProducaoRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.mockito.ArgumentCaptor;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest(
-        properties = {
-                "integration.pedidos.url=http://localhost:9999"
-        }
-)
 class PedidoPagoListenerTest {
-
-    // 👉 ISSO resolve o erro do JwtDecoder
-    @MockBean
-    private JwtDecoder jwtDecoder;
-
-    @Autowired
-    private PedidoPagoListener listener;
-
-    @Autowired
-    private PedidoProducaoRepository repository;
-
-    @BeforeEach
-    void setup() {
-        repository.deleteAll();
-    }
 
     @Test
     void deve_salvar_pedido_com_status_recebido_e_idPedidoOriginal() {
+        // arrange
+        PedidoProducaoRepository repository = mock(PedidoProducaoRepository.class);
+        PedidoPagoListener listener = new PedidoPagoListener(repository);
+
         var item = new PedidoPagoEvento.ItemEvento("Hambúrguer", 10);
         var evento = new PedidoPagoEvento(13L, List.of(item));
 
+        // act
         listener.receber(evento);
 
-        var pedidos = repository.findAll();
-        assertThat(pedidos).hasSize(1);
+        // assert
+        ArgumentCaptor<PedidoProducao> captor = ArgumentCaptor.forClass(PedidoProducao.class);
+        verify(repository, times(1)).save(captor.capture());
 
-        PedidoProducao salvo = pedidos.get(0);
+        PedidoProducao salvo = captor.getValue();
 
         assertThat(salvo.getId()).isEqualTo("13");
         assertThat(salvo.getIdPedidoOriginal()).isEqualTo(13L);
         assertThat(salvo.getStatus()).isEqualTo(StatusPedido.RECEBIDO);
+
         assertThat(salvo.getItens()).hasSize(1);
+        assertThat(salvo.getItens().get(0).getNome()).isEqualTo("Hambúrguer");
+        assertThat(salvo.getItens().get(0).getQuantidade()).isEqualTo(10);
+
+        assertThat(salvo.getDataEntrada()).isNotNull();
+        assertThat(salvo.getDataAtualizacao()).isNotNull();
     }
 }
