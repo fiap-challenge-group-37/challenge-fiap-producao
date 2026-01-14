f3e85d6f066210570fbdbb774f44c060313ad3f2
Renan Grande <renanlgrande@hotmail.com>
2026-01-13 23:21:30 -0300

    add JSON property mappings to PedidoPagoEvento fields

--- src/main/java/com/fiap/producao/domain/dto/PedidoPagoEvento.java
+++ src/main/java/com/fiap/producao/domain/dto/PedidoPagoEvento.java
@@ -1,13 +1,20 @@
 package com.fiap.producao.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record PedidoPagoEvento(
+        @JsonProperty("idPedido") // Mapeia o campo "idPedido" do JSON
         Long idPedido,
+
+        @JsonProperty("itens")    // Mapeia o campo "itens" do JSON
         List<ItemEvento> itens
 ) {
     public record ItemEvento(
+            @JsonProperty("nome")       // Mapeia "nome"
             String nome,
+
+            @JsonProperty("quantidade") // Mapeia "quantidade"
             Integer quantidade
     ) {}
 }
